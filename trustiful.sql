2e07a36cbd41a35b092df46d3abc0ad6c065ca25
Marvin RAMEIX <marvin@wizishop.com>
2018-09-17 01:06:46 +0200

    adding sql database struct to import

---
+++ trustiful.sql
@@ -0,0 +1,133 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.0
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le :  lun. 17 sep. 2018 à 01:05
+-- Version du serveur :  5.7.17
+-- Version de PHP :  5.6.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `trustiful`
+--
+CREATE DATABASE IF NOT EXISTS `trustiful` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `trustiful`;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `certificate`
+--
+
+DROP TABLE IF EXISTS `certificate`;
+CREATE TABLE `certificate` (
+  `id_certificate` int(11) NOT NULL,
+  `id_website` int(11) NOT NULL,
+  `shipping_time` int(11) NOT NULL,
+  `dispute` int(11) NOT NULL DEFAULT '0',
+  `return_policy` int(11) NOT NULL,
+  `customer_service` int(11) NOT NULL,
+  `position` int(1) NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `id_user` int(11) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `firstname` varchar(255) NOT NULL,
+  `lastname` varchar(255) NOT NULL,
+  `gender` enum('H','F') NOT NULL,
+  `role` varchar(255) DEFAULT NULL,
+  `avatar` varchar(255) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `website`
+--
+
+DROP TABLE IF EXISTS `website`;
+CREATE TABLE `website` (
+  `id_website` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `id_certificate` int(11) DEFAULT NULL,
+  `url` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `phone` varchar(255) NOT NULL,
+  `rcs_number` varchar(255) NOT NULL,
+  `subscription` int(1) NOT NULL,
+  `evaluation_note` int(11) NOT NULL,
+  `screen_website` varchar(255) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `certificate`
+--
+ALTER TABLE `certificate`
+  ADD PRIMARY KEY (`id_certificate`);
+
+--
+-- Index pour la table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id_user`),
+  ADD UNIQUE KEY `email` (`email`),
+  ADD UNIQUE KEY `id_user` (`id_user`);
+
+--
+-- Index pour la table `website`
+--
+ALTER TABLE `website`
+  ADD PRIMARY KEY (`id_website`),
+  ADD UNIQUE KEY `id_user` (`id_user`),
+  ADD UNIQUE KEY `id_website` (`id_website`),
+  ADD UNIQUE KEY `url` (`url`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `certificate`
+--
+ALTER TABLE `certificate`
+  MODIFY `id_certificate` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT pour la table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+--
+-- AUTO_INCREMENT pour la table `website`
+--
+ALTER TABLE `website`
+  MODIFY `id_website` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
